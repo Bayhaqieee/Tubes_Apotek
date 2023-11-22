4a0bf727f81a2a70aed48837e4b16cf527383760
AgiAgustianDavi <agidavi6@gmail.com>
2023-11-22 23:08:03 +0700

    add kontak.php

---
+++ pembeli_kontak.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <!-- navbar  -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-success-subtle sticky-top">
+        <div class="container">
+            <a class="navbar-brand p-0 m-0 fs-2 fw-bold" href="#"><span class="text-success ">YPTA</span>potek.</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item ps-3 pe-3">
+                        <a class="nav-link " aria-current="page" href="pembeli.php">Home</a>
+                    </li>
+                    <li class="nav-item ps-3 pe-3">
+                        <a class="nav-link " href="pembeli_toko.php">Toko Obat</a>
+                    </li>
+                    <li class="nav-item ps-3 pe-3">
+                        <a class="nav-link active" href="pembeli_riwayat.php">Riwayat</a>
+                    </li>
+                    <li class="nav-item ps-3 pe-3">
+                        <a class="nav-link " href="pembeli_kontak.php">Kontak</a>
+                    </li>
+                    <li class="nav-item ps-3 pe-3">
+                        <form action="logout_user.php" method="post">
+                            <button class="btn btn-success" type="submit" name="logout" onclick="return confirm('Keluar?');">Logout</button>
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- navbar end  -->
+    <section class="container">
+        <div class="center-content">
+            <div>
+                <div class="row text-center pt-5 pb-3">
+                    <div class="col text-success">
+                        <h2>Kontak Kami!</h2>
+                    </div>
+                </div>
+                <div class="row justify-content-center align-items-center">
+                    <div class="card mb-3 me-2" style="max-width: 540px;">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <img src="img/div.jpeg" class="img-fluid rounded-start" alt="divs">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">Diva Sarasvati Azzahra</h5>
+                                    <p class="card-text">09021282227085</p>
+                                    <a href="https://instagram.com/divaasarasvati" target="_blank">
+                                        <button type="button" class="btn btn-success btn-sm">Instagram</button>
+                                    </a>
+                                    <a href="https://www.linkedin.com/in/divasarasvati/" target="_blank">
+                                        <button type="button" class="btn btn-success btn-sm">LinkedIn</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card mb-3 me-2" style="max-width: 540px;">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <img src="-.jpg" class="img-fluid rounded-start" alt="adit">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">Muhammad Aditya Bayhaqie</h5>
+                                    <p class="card-text">09021282227122</p>
+                                    <a href="https://instagram.com/Bayhaqieee" target="_blank">
+                                        <button type="button" class="btn btn-success btn-sm">Instagram</button>
+                                    </a>
+                                    <a href="https://www.linkedin.com/in/bayhaqieee/" target="_blank">
+                                        <button type="button" class="btn btn-success btn-sm">LinkedIn</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card mb-3 me-2" style="max-width: 540px;">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <img src="img/Agi.png" class="img-fluid rounded-start" alt="agi">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">Agi Agustian Davi</h5>
+                                    <p class="card-text">09021282227118</p>
+                                    <a href="https://instagram.com/agidavi_8" target="_blank">
+                                        <button type="button" class="btn btn-success btn-sm">Instagram</button>
+                                    </a>
+                                    <a href="https://www.linkedin.com/in/agi-agustian-davi-981839270/" target="_blank">
+                                        <button type="button" class="btn btn-success btn-sm">LinkedIn</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card mb-3 me-2" style="max-width: 540px;">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <img src="img/Dayat.jpg" class="img-fluid rounded-start" alt="dayat">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">Nur Hidayat</h5>
+                                    <p class="card-text">09021182227117</p>
+                                    <a href="https://instagram.com/nurhidayat_0309" target="_blank">
+                                        <button type="button" class="btn btn-success btn-sm">Instagram</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-center mt-2">
+                    <a href="https://forms.gle/GQkFjpr1YC1xp4zH6" target="_blank">
+                        <button type="button" class="btn btn-success">Butuh Bantuan?</button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
